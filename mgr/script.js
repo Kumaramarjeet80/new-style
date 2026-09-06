cf806e4c7318e057d245b9849d1e40ac5911a12e
Amarjeet kumar <122200343+Kumaramarjeet80@users.noreply.github.com>
2026-09-06 14:06:26 +0530

    Update script.js

--- mgr/script.js
+++ mgr/script.js
@@ -1,4 +1,4 @@
-var SPREADSHEET_API_URL = "https://script.google.com/macros/s/AKfycbyYcZAOWwu1ygHP19v5K3Z2ISmrTCf7JdQYHVuvzAcR1WCYXVfiYeuXJF-jlXGQC7lD/exec";
+var SPREADSHEET_API_URL = "https://script.google.com/macros/s/AKfycbz5-suwemRQAVplZXiio8Uwkr3gX548wohCMTosOXIGMXjMoU5xFszUWMJ5ZaKj3fK_3A/exec";
   var RAZORPAY_KEY_ID = "rzp_test_TYINZpDJ5bh2CP"; // <-- Replace with your live Razorpay Test Key ID
 
   var currentUser = null;
